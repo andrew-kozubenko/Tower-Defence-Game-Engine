5764e3ae7038565b3e2bea3e67c33ea3d20c6e20
Svetlana Yakutina <s.yakutina@g.nsu.ru>
2024-12-25 03:50:10 +0700

    gun

--- TowerDefence/src/main/java/ru/nsu/t4werok/towerdefence/view/game/entities/tower/TowerView.java
+++ TowerDefence/src/main/java/ru/nsu/t4werok/towerdefence/view/game/entities/tower/TowerView.java
@@ -81,6 +81,29 @@ public class TowerView {
         gc.drawImage(towerImage, pixelX, pixelY, towerWidth, towerHeight);
     }
 
+    public void renderBullets(Tower tower, int numBullets) {
+        // Координаты башни
+        double startX = (tower.getX() + 0.5) * (canvas.getWidth() / gameMap.getWidth());
+        double startY = (tower.getY() + 0.5) * (canvas.getHeight() / gameMap.getHeight());
+
+        // Координаты врага
+        double endX = tower.getAttackX();
+        double endY = tower.getAttackY();
+
+        // Вычисляем шаг между шариками
+        double deltaX = (endX - startX) / (numBullets - 1);
+        double deltaY = (endY - startY) / (numBullets - 1);
+
+        // Рисуем шарики
+        gc.setFill(Color.RED);
+        for (int i = 0; i < numBullets; i++) {
+            double bulletX = startX + i * deltaX;
+            double bulletY = startY + i * deltaY;
+
+            gc.fillOval(bulletX - 5, bulletY - 5, 10, 10); // Рисуем круг диаметром 10
+        }
+    }
+
     public void showTowerUpgradeMenu(Tower tower, List<TechTree> techTrees) {
         techTreeView.showTowerUpgradeMenu(tower, techTrees);
     }
